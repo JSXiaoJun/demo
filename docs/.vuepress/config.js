b79053a635c1edabf88a80a6e31aacecbcba14df
@liangjun <15277934126@163.com>
2023-07-27 13:27:19 +0800

    保存

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,27 @@
 /*
  * @Author: your name
  * @Date: 2023-07-14 15:59:37
- * @LastEditTime: 2023-07-25 11:15:27
+ * @LastEditTime: 2023-07-27 11:31:15
  * @LastEditors: BG1659
  * @Description: In User Settings Edit
  * @FilePath: \demo\docs\.vuepress\config.js
  */
+const moment = require("moment")
+
 module.exports = {
+  base:"/demo/",
   title: "JS小军",
   description: "JS小军",
   head: [
-    ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
+    ["link", { rel: "icon", href: "/assets/img/favicon.ico" }],
     ["meta", { name: "author", content: "JS小军" }],
-    ["meta", { name: "keyword", content: "vuepress介绍，vuepress说明，JS小军" }],
+    [
+      "meta",
+      { name: "keyword", content: "vuepress介绍，vuepress说明，JS小军" },
+    ],
   ],
   themeConfig: {
-    lastUpdated: '更新时间', // string | boolean
+    lastUpdated: "更新时间", // string | boolean
     logo: "./assets/img/hero.png",
     nav: [
       { text: "Home", link: "/" },
@@ -49,4 +55,17 @@ module.exports = {
       "/javasciprt/": ["", "js1", "js2"],
     },
   },
+  plugins: [
+    [
+      "@vuepress/last-updated",
+      {
+        transformer: (timestamp) => {
+          // 不要忘了安装 moment
+          const moment = require("moment")
+          moment.locale("zh-cn")
+          return moment(timestamp).format("LLLL")
+        },
+      },
+    ],
+  ],
 }
